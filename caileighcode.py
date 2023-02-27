3e57586c6ad3d6819ec3ef811750e5bb1b42bd1d
meefy24 <125102806+meefy24@users.noreply.github.com>
2023-02-26 22:05:11 -0500

    Update caileighcode.py

--- caileighcode.py
+++ caileighcode.py
@@ -1,5 +1,4 @@
 import time
-import random
 import sys
 
 #SPAWN STATS
@@ -9,28 +8,6 @@ power = 20
 level = 1
 stage = 0
 inventory = ['elastic band']
-monster = False
-strawberry = 0
-
-
-#SPAWN MESSAGE (Jeffrey's intro)
-print("PLANET 0:")
-print("UNKNOWN VOICE: Hey...you awake?")
-time.sleep(2)
-print("Oh, you are! Finally!")
-time.sleep(1)
-print("Welcome back, friend! You must've been knocked out when we landed on [PLANET 0].")
-time.sleep(1)
-print("I'm JEFFREY, your astronaut partner! And...you are?")
-time.sleep(1)
-print()
-name = input(">> ").upper()
-print()
-print(f"JEFFREY: Welcome, traveller {name}! Get ready for an adventure!")
-time.sleep(1)
-print("A very long one. Ten planets long, to be exact. But lucky you! You've got a hidden talent, so I'm told. What is it?")
-print()
-
 
 #SELECT SUPER POWERS
 while True:
@@ -58,44 +35,6 @@ while True:
 print()
 time.sleep(1)
 print("JEFFREY: Wow, that's amazing!")
-
-
-#OPEN STATS INTRO
-print("Mind if I view the rest of your stats?")
-while True:
-    stats_choice = input("Type [S] to view stats: ")
-    if stats_choice.lower() == 's':
-        print()
-        print('~' * 40)
-        print(f"Health: {health}/{total_health}")
-        print(f"Strength: {power}")
-        print(f"Traveller level: {level}")
-        print('~' * 40)
-        print()
-        break
-    else:
-        print("JEFFREY: Uhh that's not it. Try typing [S].")
-
-        
-#INVENTORY INTRO
-time.sleep(1)
-print(f"Jeffrey: You seem to have spawned with a backpack, traveller {name}! Might I take a peek inside?")
-inv = input("Type [I] to open inventory: ")
-if inv.lower() == 'i':
-    print()                 
-    print('~' * 40)
-    print(f"inventory contents: {inventory}")
-    print('~' * 40)
-    print()
-
-time.sleep(1)
-print("...Huh. Not a very useful weapon, but I'm sure we'll find something better.")
-time.sleep(2)
-print()
-print("Jeffrey: The stars will be setting soon, traveller. I think we should get going!")
-print("Open your menu anytime by typing [MENU]!")
-print()
-time.sleep(2)
 
 
 #ACTUAL GAME LOOP
@@ -173,100 +112,20 @@ while True:
                 power += 30
                 time.sleep(1)
                 break
-    
             elif wormfight.lower() == 'run':
                 print()
                 print("You have successfully escaped!")
-                break
-    
+                break    
             else:
                 print()
                 print("Sorry traveller, that's not a valid action.")
                 print()
-        
         #When fight/run is done
         print()
         print("JEFFREY: Nice work! Jump to the next planet whenever you're ready.")
             
-
-
-    elif stage == 2: #ANDY
-        print("PLANET 2:")
-        print("JEFFREY:Congratulations!You found the armor here.")
-        print("JEFFREY:I have automatically equipped it for you.")
-        health += 20
-        inventory.append("armor")
-
-    
-    elif stage == 3: #LEO
-        print("PLANET 3:")
-        print("A giant dragon appears before your very eyes.")
-        print("JEFFERY: AHH!")
-        print("Dragon: Hello, traveller! You have something I want. But I'm not here to fight, I'm here to TRADE.")
-        print(f"JEFFERY: Oh thank god {name}! I thought we had to fight that thing!")
-        print("Dragon: I want that elastic band of yours ... I'll offer you to pick 1 of 2 mystery boxes for it. Which one do you want?")
-        print(f"JEFFERY: Maybe the second one? I don't know. It's up to you, {name}.")
-        mystery_box = input("(1 or 2) >> ")
-        trade = random.randrange(1,3)
-        if mystery_box == trade:
-            print("Dragon: You got to trade your elastic band for an upgraded weapon!")
-            inventory.append("Metal Sword")
-            inventory.remove("Elastic Band")
-        else:
-            print("Dragon: You picked a power curse ... No take backs, I'm keeping your elastic band.")
-            print("AW HECK NO!")
-            inventory.remove("elastic band")
-            power = power - (power * 0.25)
-
-
             
-    elif stage == 4: #ANDY
-        print("PLANET 4:")
-         
-
-        #loop to repeat the question if typed wrong
-        while True:
-            choice_4=input("small monster appears to be minding its own business.Do you wanna to kill or leave?: ")
             
-            if choice_4 == "kill":
-                level += 1
-                monster = True
-                break #exits loop because typed correctly
-            elif choice_4 == "leave":
-                strawberry += 1
-                print("JEFFERY: You are a good person, here is a strawberry for you!")
-                break
-            else:
-                print("Didn't get that ... Come again?")
-      
-
-
-
-
-    elif stage == 5: #LEO
-        print("PLANET 5:")
-        print("JEFFERY: We came across a space pod!")
-        user_input = input("JEFFERY: Do you want to open the space pod?")
-        if user_input.lower() == ("yes"):
-            print("JEFFERY: You found a space tomato inside!")
-            inventory.append("space tomato")
-        else:
-            print("JEFFERY: Okay...")
-
-        
-    elif stage == 6: #TARA
-        print("PLANET 6:")
-        time.sleep(1)
-        print(f"{name} there is a galaxy medicine over there that might heal you or be poisonous")
-        print("Do you want to drink the magical GALAXY drink?")
-        galaxy=input()
-        if galaxy=="yes" or galaxy=="Yes":
-            print("curse is healed, you got your ELASTIC BAND back:>")
-            inventory.append("Elastic Band")  
-        if galaxy=="No" or galaxy=="no":
-             print("OH NO, GALAXY drink was not poisonous")
-            
-
     elif stage == 7: #CAILEIGH
         print("PLANET 7:")
         #dialogue
@@ -323,7 +182,6 @@ while True:
                     break
                 else:
                     print("Sorry traveller, that wasn't a valid command!")
-
         else:
             print("You check your inventory...")
             time.sleep(1)
@@ -337,39 +195,7 @@ while True:
             print()
             jeffrey_saved = False
 
-    
-
-    elif stage == 8: #TARA
-       print("PLANET 8:")
-       time.sleep(1)
-       print("Wow! look at those monkeys")
-       print(f"Hey, {name} will you fight or run?")
-       monkeysfight=input()
-       if monkeysfight=="fight" or monkeysfight=="Fight":
-            print("Congratulation you got the mana banana:>")
-            print("Your power increasing...")
-            power+=10
-            print(f"current power: {power}")
-            if monkeysfight=="Run"or monkeysfight=="run":
-                print("Oh no, you missed the chance to win the mana banana")
-             
-        
-
-    elif stage == 9: #TARA
-        print("PLANET 9:")
-        time.sleep(1)
-        print(f"Welcome to the next planet {name}")
-        print("look there is a map over there that might help you")
-        time.sleep(1)
-        print("Here you go")
-        time.sleep(1)
-        print(f"{name} do you want to check the map?")
-        map=input()
-        if map=="yes" or map=="Yes":
-              print("Looks like that a giant octopus is on the next planet, GOOD LUCK!")
-        if map=="no" or map=="No":
-             print(f"Emmm, ok {name} GOOD LUCK!")
-
+            
     
     elif stage == 10: #CAILEIGH
         print("PLANET 10:")
